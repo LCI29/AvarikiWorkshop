c5c734f3138852c5f7cfb0414031b3e8cd6994a8
Clement Lenskiy <blind-mind@yandex.ru>
2022-04-17 23:55:37 +0300

    Data done. Need make test start as extension for Case

---
+++ src/main/kotlin/Ext.kt
@@ -0,0 +1,3 @@
+fun Case.test() {
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,15 @@ import Decision.*
 
 class Situation(
     val text: String,
-    val rightDecision: List<Decision>,
+    val rightDecision: List<Decision> = listOf(),
     val next: MutableList<Situation> = mutableListOf()
 ) {
     fun add(situation: Situation) = next.add(situation)
+
+    fun attachTo(situation: Situation): Situation {
+        situation.add(this)
+        return this
+    }
 }
 
 class Case(
@@ -30,7 +35,7 @@ fun main() {
             STOP_TRAIN_ON_GOOD_PLACE,
             DISP,
         )
-    ).also { sit2.add(it) }
+    ).attachTo(sit2)
 
     val sit2_1a = Situation(
         text = "Обнаружена вспышка в головной части поезда",
@@ -39,7 +44,7 @@ fun main() {
             VO_CHECK_TKPR,
             DISP_KV_ON
         )
-    ).also { sit2_1.add(it) }
+    ).attachTo(sit2_1)
 
     val sit2_1b = Situation(
         text = "Обнаружена вспышка в хвостовой части поезда",
@@ -48,7 +53,7 @@ fun main() {
             VO_CHECK_TKPR,
             DISP_KV_ON
         )
-    ).also { sit2_1.add(it) }
+    ).attachTo(sit2_1)
 
     val sit2_2a = Situation(
         text = "После отжатия башмаков токоприемников на головной части вагонов\n" +
@@ -59,7 +64,7 @@ fun main() {
             VO_CHECK_TKPR,
             DISP_KV_ON
         )
-    ).also { sit2_1a.add(it) }
+    ).attachTo(sit2_1a)
 
     val sit2_2b = Situation(
         text = "После отжатия башмаков токоприемников на хвостовой части вагонов\n" +
@@ -70,7 +75,7 @@ fun main() {
             VO_CHECK_TKPR,
             DISP_KV_ON
         )
-    ).also { sit2_1b.add(it) }
+    ).attachTo(sit2_1b)
 
     val sit2_3 = Situation(
         text = "После отжатия башмаков токоприемников на части вагонов\n" +
@@ -80,12 +85,10 @@ fun main() {
             DROP_OFF_PASSENGERS_NEXT_STATION,
             FOLLOW_OFF
         )
-    ).also {
-        sit2_1a.add(it)
-        sit2_1b.add(it)
-        sit2_2a.add(it)
-        sit2_2b.add(it)
-    }
+    ).attachTo(sit2_1a)
+        .attachTo(sit2_1b)
+        .attachTo(sit2_2a)
+        .attachTo(sit2_2b)
 
     val sit3 = Situation(
         text = "При подаче напряжения 825 В на контактный рельс, оно\n" +
@@ -101,11 +104,179 @@ fun main() {
         rightDecision = listOf(
             PVU_DEACT_TKPR_3
         )
-    ).also { sit3.add(it) }
+    ).also {
+        sit3.add(it)
+    }.apply {
+        add(sit2_2b)
+    }
 
-    val sit3_2 = Situation(
-        text = "После отжатия башмаков токоприемников на 3 вагоне\n" +
+    val sit4_a = Situation(
+        text = "Невозможно дистанционно отжать башмаки\n" +
+                "токоприемников вагонов по причине наличия давления воздуха в НМ менее\n" +
+                "3,5 кгс/см2\n" +
+                "или неисправности привода отжатия башмаков токоприемников",
+        rightDecision = listOf(
+            DISP,
+            DISP_KV_OFF,
+            CABIN_OFF,
+            TRACK_LEFT_TKPR_OFF_1_2,
+            TRACK_RIGHT_TKPR_OFF_1_2,
+            TRACK_SHORT_OFF,
+            DISP_KV_ON
+        )
+    ).also {
+        sit2_1a.add(it)
+    }
+
+    val sit4_b = Situation(
+        text = "Невозможно дистанционно отжать башмаки\n" +
+                "токоприемников вагонов по причине наличия давления воздуха в НМ менее\n" +
+                "3,5 кгс/см2\n" +
+                "или неисправности привода отжатия башмаков токоприемников",
+        rightDecision = listOf(
+            DISP,
+            DISP_KV_OFF,
+            CABIN_OFF,
+            TRACK_LEFT_TKPR_OFF_3,
+            TRACK_RIGHT_TKPR_OFF_3,
+            TRACK_SHORT_OFF,
+            DISP_KV_ON,
+        )
+    ).also {
+        sit2_1b.add(it)
+    }
+
+    val sit4_c = Situation(
+        text = "Невозможно дистанционно отжать башмаки\n" +
+                "токоприемников вагонов по причине наличия давления воздуха в НМ менее\n" +
+                "3,5 кгс/см2\n" +
+                "или неисправности привода отжатия башмаков токоприемников",
+        rightDecision = listOf(
+            DISP,
+            DISP_KV_OFF,
+            CABIN_OFF,
+            TRACK_LEFT_TKPR_OFF_3,
+            TRACK_RIGHT_TKPR_OFF_3,
+            TRACK_SHORT_OFF,
+            DISP_KV_ON
+        )
+    ).also {
+        sit3_1.add(it)
+    }
+
+    val sit5_1a = Situation(
+        text = "После отжатия башмаков токоприемников на 1 и 2 вагонах состава\n" +
                 "напряжение 825 В на контактный рельс не подается",
-        rightDecision = listOf()
+        rightDecision = listOf(
+            DISP_KV_OFF,
+            CABIN_OFF,
+            TRACK_LEFT_TKPR_ON_1_2,
+            TRACK_LEFT_TKPR_OFF_3,
+            TRACK_RIGHT_TKPR_OFF_3,
+            TRACK_RIGHT_TKPR_ON_1_2,
+            TRACK_SHORT_OFF,
+            DISP_KV_ON,
+        )
     )
+
+    val sit5_1bc = Situation(
+        text = "После отжатия башмаков токоприемников на 3 вагоне состава\n" +
+                "напряжение 825 В на контактный рельс не подается",
+        rightDecision = listOf(
+            DISP_KV_OFF,
+            CABIN_OFF,
+            TRACK_LEFT_TKPR_OFF_1_2,
+            TRACK_LEFT_TKPR_ON_3,
+            TRACK_RIGHT_TKPR_ON_3,
+            TRACK_RIGHT_TKPR_OFF_1_2,
+            TRACK_SHORT_OFF,
+            DISP_KV_ON,
+        )
+    ).also {
+        sit4_b.add(it)
+        sit4_c.add(it)
+    }
+
+    val sit5_a = Situation(
+        text = "После отжатия башмаков токоприемников на 1 и 2 вагонах\n" +
+                "напряжение 825 В на контактный рельс подалось и не снимается",
+        rightDecision = listOf(
+            FOLLOW_35,
+            DROP_OFF_PASSENGERS_NEXT_STATION,
+            FOLLOW_OFF,
+        )
+    ).also {
+        sit4_a.add(it)
+        sit5_1bc.add(it)
+    }
+
+    val sit5_bc = Situation(
+        text = "После отжатия башмаков токоприемников на 3 вагоне\n" +
+                "напряжение 825 В на контактный рельс подалось и не снимается",
+        rightDecision = listOf(
+            FOLLOW_35,
+            DROP_OFF_PASSENGERS_NEXT_STATION,
+            FOLLOW_OFF,
+        )
+    ).also {
+        sit4_b.add(it)
+        sit4_c.add(it)
+        sit5_1a.add(it)
+    }
+
+    val sit6 = Situation(
+        text = "По информации поездного диспетчера напряжение 825 В на\n" +
+                "контактном рельсе есть",
+        rightDecision = listOf(
+            VO_CHECK_TKPR,
+            VPU_CHECK_TKPR,
+        )
+    ).also {
+        sit3.add(it)
+    }
+
+    val sit6_1 = Situation(
+        text = "По монитору машиниста выявлено отжатие токоприемников на\n" +
+                "всем составе",
+        rightDecision = listOf(
+            PVZ_1_TKPR_OFF,
+            KV_CHECK_825,
+        )
+    ).also {
+        sit6.add(it)
+    }
+
+    val sit6_2 = Situation(
+        text = "На головном вагоне напряжение 825 В появилось",
+        rightDecision = listOf(
+            PVZ_2_3_TKPR_OFF
+        )
+    ).also {
+        sit6_1.add(it)
+    }
+
+    val sit6_3 = Situation(
+        text = "Напряжение 825 В восстановилось на всех вагонах",
+        rightDecision = listOf(
+            DISP,
+            FOLLOW_WORK
+        )
+    ).also {
+        sit6_2.add(it)
+    }
+
+    val sitFinish = Situation(
+        text = "Случай завершен."
+    )
+        .attachTo(sit6_3)
+        .attachTo(sit5_a)
+        .attachTo(sit5_bc)
+        .attachTo(sit2_3)
+
+    val case = Case(
+        name = "Снятие высокого напряжения на выбеге",
+        startSituation = sit2
+    ).test()
+    // Start case
+
 }
